407dd22b0025ad70b488747e5c61aa458987c1e2
Dianaaraujosilva <102118579+Dianaaraujosilva@users.noreply.github.com>
2022-04-05 19:14:40 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,22 @@ namespace CalculadoraIMC
 
             Console.WriteLine("{0} sua altura é {1} e seu peso {2}", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
+            
+            if(imc < 17){
+                Console.WriteLine("Você se encontra abaixo do peso");
+            }else if(imc > 17 && imc < 18.5){
+                Console.WriteLine("Você se encontra no peso ideal");
+            }else if(imc > 18.5 && imc < 25){
+                Console.WriteLine("Você está levemente acima do peso");
+            }else if(imc > 25 && imc < 30){
+                Console.WriteLine("Você se encontra com obesidade grau I");
+            }else if(imc > 30 && imc < 35){
+                Console.WriteLine("Você se encontra com obesidade grau II");
+            }else if(imc > 35 && imc < 40){
+                Console.WriteLine("Você se encontra com obesidade grau III ou mórbida");
+            }else  if (imc > 40){
+                Console.WriteLine("Obesidade Grau III ou mórbida");
+            }
         }
     }
 }
